054f422c686143e703a33613abb1a6931dbe9bd2
prpinkas <apinkas@bmcc.cuny.edu>
2023-04-26 09:13:07 -0400

    Add files via upload

---
+++ p5Example/empty-example/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+createCanvas(500,500);
+background(255,50,50);
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(width/2, height/2, 100, 100);
+}
